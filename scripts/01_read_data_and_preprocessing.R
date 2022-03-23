d988deb86f789efe3c7a9563dcc11b8b6a9f6d45
BADOL Coline <cbbiologie@gmail.com>
2022-03-23 16:57:57 +0100

    maj 01_read_data_and_preprocessing

--- scripts/01_read_data_and_preprocessing.R
+++ scripts/01_read_data_and_preprocessing.R
@@ -249,8 +249,9 @@ data <- bind_rows(wama,
   mutate_if(is.character, as.factor) %>%
   group_by_at(vars(-effectif)) %>% 
   summarise(effectif = sum(effectif, na.rm = TRUE)) %>% 
-  ungroup() %>% 
-  st_as_sf(coords = c("x_wgs84", "y_wgs84"),
+  ungroup()
+
+st_as_sf(coords = c("x_wgs84", "y_wgs84"),
            crs = 4326) %>%
   filter(annee > 2010 |
            is.na(annee)) # suppression des données anciennes de aspe / wama
