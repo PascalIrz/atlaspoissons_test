51a57d8e17136996162c5abcc33af90329fc90ee
BADOL Coline <cbbiologie@gmail.com>
2022-03-24 15:51:07 +0100

    ajout fonction ajout_asbence() dans 01b_ajouter_absence

--- scripts/01b_ajouter_absence.R
+++ scripts/01b_ajouter_absence.R
@@ -1,59 +1,9 @@
-statut_station <- donner_statut_sp_point(data)
-
-statut_bv <- donner_statut_sp_bv(data)
-
-data %>%
-  pull(code_espece) %>% 
-  unique %>% 
-  length
-
-data %>% 
-  sf::st_drop_geometry() %>% 
-  filter(effectif == 0) %>% 
-  nrow
-
-#######################################################################
-code_valide <- passerelle_taxo %>%
-  filter(nchar(esp_code_alternatif) == 3) %>% 
-  pull(esp_code_alternatif)
-
-data2 <- data %>% 
-  mutate(ope_id = paste0(code_station,date_peche,annee,localisation,type_peche)) %>% 
-  filter(code_espece %in% code_valide) %>% 
-  droplevels()
-
-ope_id <- data2 %>% 
-  pull(ope_id) %>% 
-  unique
-
-code_espece <- data2 %>% 
-  pull(code_espece) %>% 
-  unique
-
-
-df_ope <- expand.grid(ope_id=ope_id,code_espece=code_espece)
-
-test <- df_ope %>% 
-  left_join(data2)
-
-#######################################################################
-
-ope <- data2 %>% 
-  select(-code_espece,-effectif) %>% 
-  distinct()
-
-ope %>% 
-  count(ope_id) %>% 
-  arrange(-n)
-
-test2 <- test %>% 
-  left_join(data2)
-
-#######################################################################
-
 # Fonction annotée à compléter
 
 ajout_absence <- function(df) {
+  
+  # On commence par ajouter toutes les espèces pour chaque opération 
+  # Une opération = une pêche
   
   # Permet de sélectionner les codes espèces dans le fichier "passerelle_taxo"
   # disponible dans le package aspe. On ne sélectionne que les codes "valides", 
@@ -64,26 +14,29 @@ ajout_absence <- function(df) {
   
   # On crée un code pour chaque opération
   data <- df %>% 
-    mutate(ope_id = paste0(code_station,date_peche,annee,localisation,type_peche))) %>% 
-  filter(code_espece %in% code_valide) %>% 
-  droplevels()
+    mutate(ope_id = paste0(code_station,date_peche,annee,localisation,type_peche)) %>% 
+  filter(code_espece %in% code_valide)
 
-# On récupère le code de chaque opération sous forme de vecteur
-ope_id <- data %>% 
-  pull(ope_id) %>% 
-  unique
+  # On récupère le code de chaque opération sous forme de vecteur
+  ope_id <- data %>% 
+    pull(ope_id) %>% 
+    unique
 
-# On récupère le code espèce sous forme de vecteur
-code_espece <- data %>% 
-  pull(code_espece) %>% 
-  unique
+  # On récupère le code espèce sous forme de vecteur
+  code_espece <- data %>% 
+    pull(code_espece) %>% 
+    unique
 
-# On crée un nouveau dataframe en joignant les deux vecteurs, on renomme les 
-# variables
-df_ope <- expand.grid(ope_id=ope_id,code_espece=code_espece)
+  # On crée un nouveau dataframe en joignant les deux vecteurs, on renomme les 
+  # variables
+  df_ope <- expand.grid(ope_id=ope_id,code_espece=code_espece)
 
-# On rejoint ce nouveau dataframe à l'ancien
-data <- df_ope %>% 
-  left_join(data)
+  # On rejoint ce nouveau dataframe à l'ancien
+  # Remplacer NA par 0
+  data <- df_ope %>% 
+    left_join(data) %>% 
+    mutate(effectif = if_else(is.na(effectif), 0, effectif))
 
 }
+
+ajout_absence(data)
