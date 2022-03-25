20337449ed9ca6ad32d334211363164df69cd560
BADOL Coline <cbbiologie@gmail.com>
2022-03-25 16:55:40 +0100

    maj script sp_mapping

--- scripts/sp_mapping.R
+++ scripts/sp_mapping.R
@@ -1,9 +1,41 @@
-sp_mapping <- function (data_sf, sp) {
-  
-  # juste mis les deux fonctions qu'on a créées pour l'instant 
-  
-  donner_statut_sp_point(data)
-  
-  donner_statut_sp_bv(data)
-  
-}
+# =============================================================================
+#                         CARTE POUR LES POINTS                                
+# =============================================================================
+datapt <- donner_statut_sp_point(data)
+
+datapt <- datapt %>% 
+  left_join(data) %>% 
+  st_sf
+
+cartept <- datapt %>% 
+  sample_n(500) %>% 
+  mapview(zcol="presence", cex = "effectif", col.regions = list("green","red"))
+
+cartept
+
+# =============================================================================
+#                         CARTE POUR LES BV                                    
+# =============================================================================
+
+databv <- donner_statut_sp_bv(data)
+
+databv <- databv %>% 
+  left_join(bassins_simp) %>% 
+  st_sf
+
+cartebv <- databv %>% 
+  sample_n(500, replace = TRUE) %>% 
+  mapview(zcol="statut", col.regions = list("red","orange", "green","grey"), alpha.regions = 0.3)
+
+cartebv
+# ça n'affiche rien, juste la légende
+
+# =============================================================================
+#                         CARTE POUR LES DEUX                                  
+# =============================================================================
+
+data3_sample <- data3 %>% 
+  sample_n(500, )
+
+mapview(datapt, zcol="presence", cex = "effectif", col.regions = list("green", "red")) +
+  mapview(databv, ...)
