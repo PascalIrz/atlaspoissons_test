995579f5723e069c5fde76840d4fb5b3f40554b1
BADOL Coline <cbbiologie@gmail.com>
2022-03-30 16:57:52 +0200

    ajout script pour application

---
+++ scripts/dashboard.R
@@ -0,0 +1,7 @@
+# 2 onglets ( ou menus déroulants )
+# Welcome, carte avec sélection d'espèces (affichage points et bassins)
+# sources de données, méthodologie...
+
+# Essais
+# Bassins versants, esssayer de mettre la carte avec une liste déroulante
+# des espèces (noms en français plutôt que nom latin)
