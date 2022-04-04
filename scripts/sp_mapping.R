278e4efd86557e93a8eedb6c6daa6db6a4f0adb8
PascalIrz <pascal.irz@gmail.com>
2022-04-04 10:36:08 +0200

    clean sp_mapping

--- scripts/sp_mapping.R
+++ scripts/sp_mapping.R
@@ -1,3 +1,5 @@
+load(file = "processed_data/fish_and_geographical_data.RData")
+
 # =============================================================================
 #                         CARTE POUR LES POINTS                                
 # =============================================================================
@@ -8,14 +10,10 @@ datapt <- datapt %>%
   st_sf
 
 cartept <- datapt %>% 
-<<<<<<< HEAD
   sample_n(500) %>% 
   mapview(zcol = "presence",
           cex = "effectif",
           col.regions = list("#ff0055", "#b3e93e"))
-=======
-  mapview(zcol="presence", cex = "effectif")
->>>>>>> 1190f33872c6146a2d6951bf69c4d9f9f8ec2306
 
 cartept
 
@@ -30,13 +28,9 @@ databv <- databv %>%
   st_sf
 
 cartebv <- databv %>% 
-<<<<<<< HEAD
   filter(code_espece == "ABL") %>% 
   mapview(zcol = "statut",
           alpha.regions = 0.3)
-=======
-  mapview(zcol="statut")
->>>>>>> 1190f33872c6146a2d6951bf69c4d9f9f8ec2306
 
 cartebv
 
@@ -44,12 +38,11 @@ cartebv
 #                         CARTE POUR LES DEUX                                  
 # =============================================================================
 
-<<<<<<< HEAD
 mapview(bassins_simp, col.regions = "#8fd175") +
   mapview(datapt, zcol="presence",
           cex = "effectif",
           col.regions = list("#ff0055", "#b3e93e"))
-=======
+
 map <- mapview(databv, zcol="statut", alpha.region = 0.3) +
   mapview(datapt, zcol="presence", cex = "effectif")
->>>>>>> 1190f33872c6146a2d6951bf69c4d9f9f8ec2306
+
