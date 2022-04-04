59a08e76c65a2721f651f505d8a9a2f1ee264e1f
PascalIrz <pascal.irz@gmail.com>
2022-04-04 10:58:49 +0200

    details

--- scripts/dashboard_objets.R
+++ scripts/dashboard_objets.R
@@ -11,30 +11,30 @@ sidebar <- dashboardSidebar(
 ##### body #####
   
 
+##### maps #####
+espece1 <- "ABH"
+espece2 <- "ABL"
+databv_abh <- filter(databv,code_espece == espece1)
+datapt_abh <- filter(datapt,code_espece == espece1)
+map_abh <- mapview(databv_abh, zcol="statut", alpha.region = 0.3) +
+  mapview(datapt_abh, zcol="presence", cex = "effectif")
+
+databv_abl <- filter(databv,code_espece == espece2)
+datapt_abl <- filter(datapt,code_espece == espece2)
+map_abl <- mapview(databv_abh, zcol="statut", alpha.region = 0.3) +
+  mapview(datapt_abh, zcol="presence", cex = "effectif")
 
 ##### boxes #####
-  
+
 box1 <- box(leafletOutput("map"))
 
 box2 <- box(selectInput("select",
                         "Select smth",
-                        list("ABH" = map_abh, "ABL" = map_abl),
+                        list(espece = map_abh, "ABL" = map_abl),
                         selected = 1,
                         multiple = FALSE,
                         selectize = TRUE,
                         width = NULL,
                         size = NULL
-                        )
-            )
-
-##### maps #####
-
-databv_abh <- filter(databv,code_espece == "ABH")
-datapt_abh <- filter(datapt,code_espece == "ABH")
-map_abh <- mapview(databv_abh, zcol="statut", alpha.region = 0.3) +
-  mapview(datapt_abh, zcol="presence", cex = "effectif")
-
-databv_abl <- filter(databv,code_espece == "ABH")
-datapt_abl <- filter(datapt,code_espece == "ABH")
-map_abl <- mapview(databv_abh, zcol="statut", alpha.region = 0.3) +
-  mapview(datapt_abh, zcol="presence", cex = "effectif")
+)
+)
