082cd1b0ce43f75a3cf7e016be0aadbd5e139695
BADOL Coline <cbbiologie@gmail.com>
2022-04-05 15:20:58 +0200

    essais de selectInput

--- scripts/32_test_reactive.R
+++ scripts/32_test_reactive.R
@@ -4,8 +4,13 @@ library(mapview)
 library(leaflet)
 library(leafem)
 
+
+
 load(file = "processed_data/fish_and_geographical_data.RData")
 
+# =================================================================
+#                             DATA
+# =================================================================
 data_abl <- data %>% 
   st_drop_geometry() %>% 
   filter(code_espece == "ABL")
@@ -14,25 +19,36 @@ data_abh <- data %>%
   st_drop_geometry() %>% 
   filter(code_espece == "ABH")
 
+espece1 <- "ABH"
+espece2 <- "ABL"
+databv_abh <- filter(databv,code_espece == espece1)
+datapt_abh <- filter(datapt,code_espece == espece1)
+map_abh <- mapview(databv_abh, zcol="statut", alpha.region = 0.3) +
+  mapview(datapt_abh, zcol="presence", cex = "effectif")
+
+databv_abl <- filter(databv,code_espece == espece2)
+datapt_abl <- filter(datapt,code_espece == espece2)
+map_abl <- mapview(databv_abh, zcol="statut", alpha.region = 0.3) +
+  mapview(datapt_abh, zcol="presence", cex = "effectif")
+
+# =================================================================
+#                        TEST SHINY
+# =================================================================
+
 ui <- fluidPage(
   
   selectInput("select",
               "Select smth",
-              list(`Able de Heckel` = data_abh, Ablette = data_abl),
-              multiple = FALSE,
-              selectize = TRUE,
-              width = NULL,
-              size = NULL),
+              list(`Able de Heckel` = map_abh, Ablette = map_abl)),
   
   hr(),
-  fluidRow(column(3, dataTableOutput("data"))))
+  fluidRow(box(leafletOutput("map"))))
 
 server <- function(input, output) {
   
-  data <- reactive(input$select)
+  data <- reactive(input$map)
   
-  output$data = DT::renderDataTable(
-    DT::datatable(data()))
+  output$select = renderLeaflet({ data() })
 }
 
 shinyApp(ui,server)
