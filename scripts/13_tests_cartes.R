9845e0807f5228e9735af9e7a833c099386ed546
BADOL Coline <cbbiologie@gmail.com>
2022-05-04 16:29:27 +0200

    maj des fichiers 11 et 13

--- scripts/13_tests_cartes.R
+++ scripts/13_tests_cartes.R
@@ -7,25 +7,36 @@ rm(list = ls())
 load(file = "../atlas_poissons_app/atlas/donnees_appli.RData")
 
 mon_espece <- "Lamproie marine"
-premiere_annee <- 2010
-derniere_annee <- 2021
+premiere_annee <- 2021
+derniere_annee <- 2018
 
+
+# Ici, ça enlève 3/4 des données, on se retrouve avec 40124 observations
+# Alors que dans bv_map_data, il y a 199004 observations
+# Le mutate ne fonctionne donc pas, les bv non prospectés n'apparaissent pas
 bv <- bv_map_data %>% 
+  group_by(code_exutoire, code_espece, annee, esp_nom_commun) %>%
   summarise(n_abs = sum(statut == "Absent"),
             n_pres = sum(statut == "Présent"),
-            n_n_d = sum(statut == "Non détecté")) %>% 
+            n_n_d = sum(statut == "Non détecté"),
+            n_np = sum(statut == "Non prospecté")) %>% 
   ungroup() %>% 
   mutate(statut = case_when(
     n_pres > 0 ~ "Présent",
     n_pres == 0 & n_abs > 0 ~ "Absent",
     TRUE ~ "Non détecté"
-  ))
+  )) 
 
+bv %>% 
+  mutate(statut = ifelse(is.na(statut), "Non prospecté", statut),
+         statut = as.factor(statut),
+         statut = fct_relevel(statut, c("Présent", "Absent", "Non détecté", "Non prospecté" )))
 
-bv_geo <- bv_map_geo %>% 
-  left_join(bv)
+bv %>% 
+  left_join(bv_map_geo)
 
 pt <- pt_data%>%
+  group_by(code_coords, code_exutoire, esp_nom_commun, code_espece, code_station, annee) %>%
   summarise(n_an_abs = sum(statut == "Absent"),
             n_an_pres = sum(statut == "Présent"),
             n_an_n_d = sum(statut == "Non détecté")) %>%
@@ -35,19 +46,17 @@ pt <- pt_data%>%
     n_an_pres == 0 & n_an_abs > 0 ~ "Absent",
     TRUE ~ "Non détecté"
   ))
-# Ne donne plus qu'1 observation
 
 pt_g <- pt_geo %>%
   left_join(pt)   %>% 
   st_sf
-# Fait tout planter
 
 pt_g <- pt_g %>% 
   filter(esp_nom_commun == mon_espece,
        annee >= premiere_annee,
        annee <= derniere_annee)
 
-bv_geo <- bv_geo %>%
+bv_geo <- bv %>%
   filter(esp_nom_commun == mon_espece,
          annee >= premiere_annee,
          annee <= derniere_annee)
@@ -56,7 +65,7 @@ mapview(bv_geo,
         zcol = "statut",
         layer.name = mon_espece,
         map.types = c("OpenStreetMap", "Esri.WorldImagery"),
-        col.regions = c("green", "red", "pink", "grey50"),
+        col.regions = c("red", "pink", "green"),
         alpha.regions = 0.5) + 
   mapview(pt_g,
           zcol = "statut",
