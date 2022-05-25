9e16b3a73dbe308b247c30389870e02b5a6c3a53
BADOL Coline <cbbiologie@gmail.com>
2022-05-25 16:13:54 +0200

    maj 15_bv_macroecologie

--- scripts/15_bv_macroecologie.R
+++ scripts/15_bv_macroecologie.R
@@ -16,6 +16,22 @@ test <- test %>%
   group_by(code_exutoire, code_espece) %>% 
   filter(eff > 0) %>% 
   mutate(presence = "1") %>% 
-  select(code_exutoire,code_espece,presence)
+  select(code_exutoire,code_espece,presence) %>% 
+  ungroup()
  # on enlève par contre tout le reste, on a juste les présences marquées d'un 1
+
+code_exutoire <- test %>%
+  pull(code_exutoire) %>%
+  unique()
+
+code_espece <- test %>% 
+  pull(code_espece) %>% 
+  unique()
+
+test2 <- test %>% 
+  select(code_espece, presence) %>% 
+  pivot_wider(names_from = "code_espece", 
+              values_to = "presence", 
+              values_fill = 0)
+# Ne fonctionne pas, normal parce qu'il faut
 
